16a87f77048ee76ddd4a092b67ea4b767e43d61b
Igor Tudisco <igortudisco@hotmail.com>
2021-11-23 18:53:07 -0300

    Initial project

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController] // Indicando que essa classe é um controlador.
+    [Route("[Controller]")] // Indicando que o meu end point vai ter o mesmo nome da minha class
+    public class FilmeController : ControllerBase // Extendendo a class.
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost] // Indicação de um verbo post
+        public void AdicionaFilme(Filme filme)
+        {
+
+            filmes.Add(filme);
+            Console.WriteLine(filme);
+
+        }
+
+    }
+}
