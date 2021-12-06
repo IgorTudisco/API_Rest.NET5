32d6da617358a8aab146979612afe64cfc5aaec5
Igor Tudisco <igortudisco@hotmail.com>
2021-12-06 11:23:15 -0300

    Add DTO de Create, Read e Update.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using FilmesAPI.Data;
+using FilmesAPI.Data.DTOs;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,24 +16,34 @@ namespace FilmesAPI.Controllers
     {
         // Usando a propriedade contexto, para acessar o DB.
 
-        private FilmeContext _comtext;
+        private FilmeContext _context;
 
         public FilmeController(FilmeContext filmeContext)
         {
-            _comtext = filmeContext;
+            _context = filmeContext;
         }
 
         // Indicação de um verbo post
         [HttpPost]
         // [FromBody] Indicação que os dados vem no corpo da requidição.
         // IActionResult Tipo dos estados http.
-        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        // Passando uma DTO e fazendo a sua converção.
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDTO filmeDTO)
         {
+            // Fazendo a converção.
+            Filme filme = new Filme
+            {
+                Titulo = filmeDTO.Titulo,
+                Genero = filmeDTO.Genero,
+                Duracao = filmeDTO.Duracao,
+                Diretor = filmeDTO.Diretor
+            };
+
             // Acessando os dados no DB. Para guardar um dado.
-            _comtext.FilmesDB.Add(filme);
+            _context.FilmesDB.Add(filme);
 
             // Salvando as alterações no DB.
-            _comtext.SaveChanges();
+            _context.SaveChanges();
 
             // Indicação de retorno do obj. Ele irá retornar o obj criado.
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
@@ -42,19 +53,79 @@ namespace FilmesAPI.Controllers
         [HttpGet]
         public IEnumerable<Filme> RecuperaFilmes()
         {
-            return _comtext.FilmesDB;
+            return _context.FilmesDB;
         }
 
         // Procurando um filme por Id.
+        // Colocando uma DTO.
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmesPorId(int id)
         {
-            Filme filme = _comtext.FilmesDB.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.FilmesDB.FirstOrDefault(filme => filme.Id == id);
             if (filme != null)
             {
-                return Ok(filme);
+                // Passando o filme para a DTO
+                ReadFilmeDTO filmeDTO = new ReadFilmeDTO
+                {
+                    Id = filme.Id,
+                    Titulo = filme.Titulo,
+                    Diretor = filme.Diretor,
+                    Duracao = filme.Duracao,
+                    Genero = filme.Genero,
+                    HoraDaConsulta = DateTime.Now
+                };
+
+                return Ok(filmeDTO);
             }
             return NotFound();
+        }
+
+        // Método de atualização.
+        // Passando um DTO
+        [HttpPost("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDTO filmeDTO)
+        {
+            // Verificando se existe o filme no DB.
+            Filme filme = _context.FilmesDB.FirstOrDefault(filme => filme.Id == id);
+            
+            // Verificação simples se o filme está nulo.
+            if (filme == null)
+            {
+                return BadRequest();
+            }
+
+            filme.Titulo = filmeDTO.Titulo;
+            filme.Genero = filmeDTO.Genero;
+            filme.Duracao = filmeDTO.Duracao;
+            filme.Diretor = filmeDTO.Diretor;
+            _context.SaveChanges();
+
+            // A boa pratica no Put é não retornar nada.
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            // Verificando se existe o filme no DB.
+            Filme filme = _context.FilmesDB.FirstOrDefault(filme => filme.Id == id);
+
+            // Verificação simples se o filme está nulo.
+            if (filme == null)
+            {
+                return BadRequest();
+            }
+
+            // Removendo um item no DB.
+            _context.Remove(filme);
+
+            // Salvando as alterações.
+            _context.SaveChanges();
+
+            // A boa pratica diz que não devemos retornar nada.
+            return NoContent();
+
         }
 
     }
